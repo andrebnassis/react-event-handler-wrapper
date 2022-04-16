5190d1355d6a7fd54e2f2d7fdc01c6b215779f60
andrebnassis <andrebnassis@gmail.com>
2022-04-15 23:24:00 -0300

    add documentation on storybook and fill repo's README

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Event Handler Wrapper Library
+https://www.npmjs.com/package/react-event-handler-wrapper
+
+This library aims to help you dealing with events on more than one component and manage external events from a target.
+
+## Documentation
+
+Full documentation at: https://andrebnassis.github.io/react-event-handler-wrapper
+ - Installation
+ - Getting Started
+ - Contribuiting
--- src/stories/out-click-event-wrapper/OutClickEventWrapper.stories.tsx
+++ src/stories/out-click-event-wrapper/OutClickEventWrapper.stories.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import { Story, Meta } from "@storybook/react";
-import { action }  from "@storybook/addon-actions"
+import { Story } from "@storybook/react";
 import OutClickEventWrapper from '../../react-event-handler-wrapper/out-click-event-wrapper/OutClickEventWrapper'
 
 
-export default {
-  title: "OutClickEventWrapper",
-  component: OutClickEventWrapper,
-  parameters:{
-    controls:{
-      disabled: true
-    },
-  }
-} as Meta;
-
 export const OutClickEvent:Story = () => {
+
+const [clickedOutside, setClickedOutside] = React.useState<boolean|undefined>(undefined);
 return (
-  <OutClickEventWrapper outClickCallback={action("callback")}>
-    <div style={{color: 'green', fontSize: '32px', backgroundColor: "#111169", textAlign:"center"}} >click inside or outside this 'div' and see the result on 'Actions' tab</div>
+  <>
+  <OutClickEventWrapper outClickCallback={(value:boolean) => setClickedOutside(value)}>
+    <div style={{
+      color: 'green', 
+      fontSize: '32px', 
+      backgroundColor: "#111169", 
+      textAlign:"center"
+    }} >
+          Click inside and outside this div to see the result
+      </div>
   </OutClickEventWrapper>
+  <p>Clicked Outside: {JSON.stringify(clickedOutside)}</p>
+  </>
 )
   
 }
